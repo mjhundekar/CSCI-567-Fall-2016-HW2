263c313098d7283d2a5fd820780b572dc3d422f8
mjhundekar <mohammedjunaidh@gmail.com>
2016-09-29 03:32:11 -0700

    added file for ridge regression

---
+++ Ridge_regression.m
@@ -0,0 +1,27 @@
+function [ W_out, Y_pred ] = Ridge_regression( X_in, Lamda, Y_in )
+%Ridge_regression Summary of this function goes here
+%   Detailed explanation goes here
+
+X_t = transpose(X_in);
+X_X_t = X_t * X_in;
+sz = size(X_X_t);
+L_I = Lamda * eye(sz);
+
+% X_X_t_inv = inv(X_X_t);
+W_out = (X_X_t + L_I) \ X_t * Y_in;
+
+W_out = transpose(W_out);
+% Replace inv(A)*b with A\b
+% Replace b*inv(A) with b/A
+Y = [];
+for i =1:length(X_in(:,1))
+    temp_y = W_out * transpose(X_in(i,:));
+    Y = [Y;temp_y];
+end
+% Should not be transpose as W_out[14,1] X_in[433,14]
+% Y = transpose(W_out) * X_in;
+% Y_pred = sum(Y);
+
+Y_pred = Y;
+end
+
