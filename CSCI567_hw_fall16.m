53820d34b5e42468487894f6a703e4c1cea12d77
mjhundekar <mohammedjunaidh@gmail.com>
2016-09-29 04:38:46 -0700

    Implemented RR using CV

--- CSCI567_hw_fall16.m
+++ CSCI567_hw_fall16.m
@@ -19,8 +19,8 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% % length(o_train) + length(o_test)
-% %  col_name{2}
+% % % length(o_train) + length(o_test)
+% % %  col_name{2}
 
 %PLOT HISTOGRAMS AND CALCULATE PERSON CORELLATION
 
@@ -60,8 +60,6 @@ MSE_lr_train = ( sum( (Y_train_true - Y_pred_train).^2) ) / length(X_norm_train(
 Res_train(1,1) = str_res(1);
 Res_train(1,2) = cellstr(num2str(MSE_lr_train));
 
-% fprintf('\nThe MSE for linear regression for training set is :: %f\n\n',MSE_lr_train);
-
 %Ridge Regression for TRAIN
 W_train_rr = [];
 for i =1:length(L)
@@ -71,8 +69,6 @@ for i =1:length(L)
     MSE_rr_train = ( sum( (Y_train_true - Y_pred_train_rr).^2) ) / length(X_norm_train(:,1));
     Res_train(i+1,1) = str_res(i+1);
     Res_train(i+1,2) = cellstr(num2str(MSE_rr_train));
-    
-    %     fprintf('\nThe MSE for ridge regression for Lamda = %f on training set is :: %f\n\n',L(i),MSE_rr_train);
 end
 
 fprintf('_____________________________________________________________________\n');
@@ -87,6 +83,7 @@ Res_test = cell(4,2);
 X_norm_test = o_test(:,1:13);
 Y_test_true = o_test(:,14);
 
+%Normalizing Test Data
 for i = 1: length(X_norm_test(:,1))
     X_norm_test(i,:) = ((X_norm_test(i,:) - mu_train))./(sigma_train);
 end
@@ -96,28 +93,25 @@ o_1_test = ones(sz_test);
 X_norm_test = [o_1_test,X_norm_test];
 
 
-%Linear Regression for TEST
+%Linear Regression for TEST based in W learned in training
 Y_lr_test = [];
 for i =1:length(X_norm_test(:,1))
     temp_y = W_train_lr * transpose(X_norm_test(i,:));
     Y_lr_test = [Y_lr_test;temp_y];
 end
-% [W_test, Y_pred_test] = Linear_regression(X_norm_test, Y_test_true);
 
 MSE_lr_test = ( sum( (Y_test_true - Y_lr_test).^2) ) / length(X_norm_test(:,1));
 Res_test(1,1) = str_res(1);
 Res_test(1,2) = cellstr(num2str(MSE_lr_test));
-
-% fprintf('\nThe MSE for linear regression for testing set is :: %f\n\n',MSE_lr_test);
 
 
 %Ridge Regression for TEST
 
 for i =1:length(L)
     Y_rr_test = [];
-%     [W_train_rr, Y_pred_test_rr] = Ridge_regression(X_norm_test, L(i), Y_test_true);
     
     W_rr_l = W_train_rr(i,:);
+    
     for j =1:length(X_norm_test(:,1))
         temp_y = W_rr_l * transpose(X_norm_test(j,:));
         Y_rr_test = [Y_rr_test;temp_y];
@@ -126,8 +120,6 @@ for i =1:length(L)
     MSE_rr_test = ( sum( (Y_test_true - Y_rr_test).^2) ) / length(X_norm_test(:,1));
     Res_test(i+1,1) = str_res(i+1);
     Res_test(i+1,2) = cellstr(num2str(MSE_rr_test));
-    
-    %fprintf('\nThe MSE for ridge regression for Lamda = %f on testing set is :: %f\n\n',L(i),MSE_rr_test);
 end
 
 fprintf('\n\n_____________________________________________________________________\n');
@@ -140,61 +132,73 @@ fprintf('_____________________________________________________________________\n
 
 %Ridge Regression with Cross-Validation
 %10 fold cross validation
+
+cv_lamda = 0.0001;
+Res_cv = [];
+while(abs(cv_lamda - 10) > 0.0001)
+    %retain 43 rows 7 times and 44 rows 3 times
+    
+    index = 1;
+    for i=1:10
+        cv_X_norm_train = X_norm_train;
+        cv_Y_train_true = Y_train_true;
+        if(i<=7)
+            %extract the test data
+            cv_X_test = X_norm_train(index:index+42,:);
+            cv_Y_test_true = Y_train_true(index:index+42);
+            %delete the extracted data from training set
+            cv_X_norm_train(index:index+42,:) = [];
+            cv_Y_train_true(index:index+42) = [];
+            index = index + 43;
+        else
+            cv_X_test = X_norm_train(index:index+43,:);
+            cv_Y_test_true = Y_train_true(index:index+43);
+            cv_X_norm_train(index:index+43,:) = [];
+            cv_Y_train_true(index:index+43) = [];
+            index = index + 44;
+        end
+        
+        [W_rr, Y_pred_test_rr] = Ridge_regression(cv_X_norm_train, cv_lamda, cv_Y_train_true);
+        Y_rr_test_cv = [];
+        for k =1:length(cv_X_test(:,1))
+            temp_y = W_rr * transpose(cv_X_test(k,:));
+            Y_rr_test_cv = [Y_rr_test_cv;temp_y];
+        end
+        
+        MSE_rr_test = ( sum( (cv_Y_test_true - Y_rr_test_cv).^2) ) / length(cv_X_test(:,1));
+        Res_cv = [Res_cv; cv_lamda,MSE_rr_test];
+        
+    end
+    
+    cv_lamda = cv_lamda * 10;
+end
+
+fprintf('\n Results of Cross validation::\n Lamda Value \t MSE\n');
+disp(Res_cv);
+
+
+% [B, FitInfo] = lasso(X_norm_train,Y_train_true,'CV',10);
+% lassoPlot(B,FitInfo,'PlotType','CV');
+
+
 %(43)x7 + (44)x3
 
-% Train_cell = mat2cell([X_norm_train,Y_train_true],[43,43,43,43,43,43,43,44,44,44],[14,1]);
-%
-% Train_cell{1,1}
-% Train_cell{1,2}
+% % % Train_cell = mat2cell([X_norm_train,Y_train_true],[43,43,43,43,43,43,43,44,44,44],[14,1]);
+% % %
+% % % Train_cell{1,1}
+% % % Train_cell{1,2}
 
-% index = 1
-% for i=1:10
-%         if(i<=7)
-%            cv_X_test = X_norm_train(index:index+42,:);
-%            index = index + 43
-%
-%         else
-%             cv_X_test = X_norm_train(index:index+43,:);
-%             index = index + 44
-%         end
-% end
-
-
+% % % index = 1
+% % % for i=1:10
+% % %         if(i<=7)
+% % %            cv_X_test = X_norm_train(index:index+42,:);
+% % %            index = index + 43
+% % %
+% % %         else
+% % %             cv_X_test = X_norm_train(index:index+43,:);
+% % %             index = index + 44
+% % %         end
+% % % end
 
 
 
-
-
-% 
-% 
-% cv_lamda = 0.0001;
-% Res_cv = [];
-% while(abs(cv_lamda - 10) > 0.00001)
-%     %retain 43 rows 7 times and 44 rows 3 times
-%     cv_X_norm_train = X_norm_train;
-%     cv_Y_train_true = Y_train_true;
-%     index = 1;
-%     for i=1:10
-%         if(i<=7)
-%             %extract the test data
-%             cv_X_test = X_norm_train(index:index+42,:);
-%             cv_Y_test_true = Y_train_true(index:index+42);
-%             %delete the extracted data from training set
-%             cv_X_norm_train(index:index+42,:) = [];
-%             cv_Y_train_true(index:index+42) = [];
-%             index = index + 43;
-%         else
-%             cv_X_test = X_norm_train(index:index+43,:);
-%             cv_Y_test_true = Y_train_true(index:index+43);
-%             cv_X_norm_train(index:index+43,:) = [];
-%             cv_Y_train_true(index:index+43) = [];
-%             index = index + 44;
-%         end
-%         
-%         [W_test_rr, Y_pred_test_rr] = Ridge_regression(cv_X_norm_train, cv_lamda, cv_Y_train_true);
-%         MSE_rr_test = ( sum( (cv_Y_train_true - Y_pred_test_rr).^2) ) / length(cv_X_norm_train(:,1));
-%         Res_cv = [Res_cv; cv_lamda,MSE_rr_test];
-%         
-%     end
-%     cv_lamda = cv_lamda *2;
-% end
--- Ridge_regression.m
+++ Ridge_regression.m
@@ -3,12 +3,12 @@ function [ W_out, Y_pred ] = Ridge_regression( X_in, Lamda, Y_in )
 %   Detailed explanation goes here
 
 X_t = transpose(X_in);
-X_X_t = X_t * X_in;
-sz = size(X_X_t);
+X_t_X = X_t * X_in;
+sz = size(X_t_X);
 L_I = Lamda * eye(sz);
 
-% X_X_t_inv = inv(X_X_t);
-W_out = (X_X_t + L_I) \ X_t * Y_in;
+X_t_X_LI_inv = pinv(X_t_X + L_I);
+W_out = X_t_X_LI_inv * X_t * Y_in;
 
 W_out = transpose(W_out);
 % Replace inv(A)*b with A\b
